a35199b42e89960ed2b338c3e8fcfe9a1edd156b
prathamesh <prathmesh6464@gmail.com>
2020-05-27 15:20:17 +0530

    REFACTORED : Added linked hash set object and it's methods.

--- src/com/bridz/collection/linked_hash_set/LinkedHashSetProgram.java
+++ src/com/bridz/collection/linked_hash_set/LinkedHashSetProgram.java
@@ -1,22 +1,33 @@
 package com.bridz.collection.linked_hash_set;
 
 import java.util.LinkedHashSet;
+import java.util.Vector;
 
 public class LinkedHashSetProgram {
 
 	public static void main(String args[]) {
 
 		LinkedHashSet linkedHashSet = new LinkedHashSet();
+		Vector vector = new Vector();
 
 		for (int i = 1; i < 6; i++) {
 
 			linkedHashSet.add(i);
+			vector.add(i);
 		}
 
+		// Showing set in order
 		linkedHashSet.forEach(data -> System.out.print(data + "   "));
 		System.out.println();
 		
-		System.out.println(linkedHashSet);
+		LinkedHashSet linkedHashSet2 = (LinkedHashSet) linkedHashSet.clone();
+		linkedHashSet2.forEach(data -> System.out.print(data + "   "));
+		System.out.println();
+		
+		linkedHashSet2 = new LinkedHashSet<>(vector);
+		linkedHashSet2.forEach(data -> System.out.print(data + "   "));
+		System.out.println();
+		
 	}
 
 }
