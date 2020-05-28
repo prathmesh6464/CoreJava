27bae0f7e2ade5f829a5067038045d13b9af2ae3
prathamesh <prathmesh6464@gmail.com>
2020-05-28 05:55:48 +0530

    ADDED : Hash map program.

--- src/com/bridz/map/hash_map/HashMapProgram.java
+++ src/com/bridz/map/hash_map/HashMapProgram.java
@@ -3,19 +3,16 @@ package com.bridz.map.hash_map;
 import java.util.HashMap;
 
 public class HashMapProgram {
-	
+
 	public static void main(String args[]) {
-		
-		HashMap hashMap =  new HashMap();
-		
-		for(int i =1; i<6; i++) {
-		
-			hashMap.put("key"+i, i);
+
+		HashMap hashMap = new HashMap();
+
+		for (int i = 1; i < 6; i++) {
+
+			hashMap.put("key" + i, i);
 		}
-		
-		
-		
-		
+
 	}
 
 }
