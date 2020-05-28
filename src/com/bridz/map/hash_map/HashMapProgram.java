a8f038f37a19faaa058e1333010d598ce5b3b1f5
prathamesh <prathmesh6464@gmail.com>
2020-05-28 12:51:23 +0530

    MODIFIED : Changed variable name in for loop from i to temporary value.

--- src/com/bridz/map/hash_map/HashMapProgram.java
+++ src/com/bridz/map/hash_map/HashMapProgram.java
@@ -2,6 +2,7 @@ package com.bridz.map.hash_map;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -10,39 +11,42 @@ public class HashMapProgram {
 
 	public static void main(String args[]) {
 
-		HashMap hashMap = new HashMap();
+		LinkedHashMap linkedHashMap = new LinkedHashMap();
 
-		for (int i = 1; i < 6; i++) {
+		for (int temporaryValue = 6; temporaryValue >= 1; temporaryValue--) {
 
-			hashMap.put("key" + i, i);
+			linkedHashMap.put("key" + temporaryValue, temporaryValue);
 		}
+		
+		System.out.println(linkedHashMap);
 
-		Set keySet = hashMap.keySet();
+		Set keySet = linkedHashMap.keySet();
 		keySet.forEach(key -> System.out.println(key));
 
-		Collection valuesOfHashMap = hashMap.values();
+		Collection valuesOfHashMap = linkedHashMap.values();
 		valuesOfHashMap.forEach(values -> System.out.println(values));
 
-		Set entrySet = hashMap.entrySet();
+		Set entrySet = linkedHashMap.entrySet();
 		entrySet.forEach(data -> {
 			Map.Entry entry = (Entry) data;
 			System.out.println("key : " + entry.getKey() + "  " + "values : " + entry.getValue());
 		});
 
-		System.out.println(hashMap.containsKey("key1"));
+		System.out.println(linkedHashMap.containsKey("key1"));
 		
-		System.out.println(hashMap.containsValue(8));
+		System.out.println(linkedHashMap.containsValue(8));
 		
-		System.out.println(hashMap.getOrDefault(7, "Key not available"));
+		System.out.println(linkedHashMap.getOrDefault(7, "Key not available"));
 		
-		System.out.println(hashMap.isEmpty());
+		System.out.println(linkedHashMap.isEmpty());
 
-		HashMap hashMap2 = (HashMap) hashMap.clone();
+		HashMap hashMap2 = (HashMap) linkedHashMap.clone();
 
 		hashMap2.entrySet().forEach(data -> {
 
 			System.out.println(((Entry) data).getKey() + "  " + ((Entry) data).getValue());
 		});
+	
 	}
 
 }
