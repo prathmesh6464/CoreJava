888e55db8c084e905bfe4d39bd8fe03d95de5b77
prathamesh <prathmesh6464@gmail.com>
2020-05-28 06:33:07 +0530

    REFACTORED : Added hash map object and has map methods.

--- src/com/bridz/map/hash_map/HashMapProgram.java
+++ src/com/bridz/map/hash_map/HashMapProgram.java
@@ -1,6 +1,10 @@
 package com.bridz.map.hash_map;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class HashMapProgram {
 
@@ -13,6 +17,32 @@ public class HashMapProgram {
 			hashMap.put("key" + i, i);
 		}
 
+		Set keySet = hashMap.keySet();
+		keySet.forEach(key -> System.out.println(key));
+
+		Collection valuesOfHashMap = hashMap.values();
+		valuesOfHashMap.forEach(values -> System.out.println(values));
+
+		Set entrySet = hashMap.entrySet();
+		entrySet.forEach(data -> {
+			Map.Entry entry = (Entry) data;
+			System.out.println("key : " + entry.getKey() + "  " + "values : " + entry.getValue());
+		});
+
+		System.out.println(hashMap.containsKey("key1"));
+		
+		System.out.println(hashMap.containsValue(8));
+		
+		System.out.println(hashMap.getOrDefault(7, "Key not available"));
+		
+		System.out.println(hashMap.isEmpty());
+
+		HashMap hashMap2 = (HashMap) hashMap.clone();
+
+		hashMap2.entrySet().forEach(data -> {
+
+			System.out.println(((Entry) data).getKey() + "  " + ((Entry) data).getValue());
+		});
 	}
 
 }
