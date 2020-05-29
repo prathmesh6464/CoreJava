31e46bae7461746f88b73d2893ca60c42afbc3d8
prathamesh <prathmesh6464@gmail.com>
2020-05-29 18:36:11 +0530

    REFACTORED : Added Generator to create all type linked list.

--- src/com/bridz/unordered_list_problem/UnorderedList.java
+++ src/com/bridz/unordered_list_problem/UnorderedList.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Collections;
 import java.util.Scanner;
 
-// Singleton pattern
 enum getInstance {
 
 	INSTANCE;
@@ -41,62 +40,54 @@ public class UnorderedList {
 			BufferedWriter bufferWriter = getInstance.INSTANCE.getBufferedWriterInstance();
 
 			String fileLines;
-			List wordList = new LinkedList();
+			List<Object> wordList = new LinkedList<Object>();
 
-			System.out.println("Enter the word to delete : ");
+			System.out.println("Enter the word : ");
 			Scanner scanner = getInstance.INSTANCE.getScannerInstance();
-			String deleteWord = scanner.next();
-
-			System.out.println("Enter the word to find : ");
 			String findWord = scanner.next();
 
-			System.out.println("Enter the word to Append : ");
-			String appendWord = scanner.next();
-
-			System.out.println("Enter the word to find index : ");
-			String findIndexOfWord = scanner.next();
-
-			System.out.println("Enter the index to inset word : ");
-			int indexPosition = scanner.nextInt();
-
-			System.out.println("Enter the word to inset on given index : ");
-			String addWordOnIndex = scanner.next();
-
 			while ((fileLines = bufferReader.readLine()) != null) {
-
 				String[] splittedFileLines = fileLines.split(" ");
 				for (String eachWord : splittedFileLines)
-					// Added new word to linked list
 					wordList.add(eachWord);
 
 			}
 
-			// Creating unordered link list using shuffle method of collections
-			//Collections.shuffle(wordList);
+			Collections.shuffle(wordList);
 
-			// Deleting word from the linked list
-			System.out.println(wordList.remove(deleteWord));
+			bufferReader.close();
 
-			// Searching value in the linked list
-			System.out.println(wordList.contains(findWord));
+			if (wordList.contains(findWord)) {
+				System.out.println(wordList);
+				wordList.remove(findWord);
+				wordList.stream().forEach(word -> {
+					try {
+						bufferWriter.write(" " + word.toString());
 
-			// Checking list is empty or not
-			System.out.println(wordList.isEmpty());
+					} catch (Exception exception) {
+						exception.printStackTrace();
+					}
+				});
 
-			// Checking size of linked list
-			System.out.println(wordList.size());
+				bufferWriter.close();
 
-			// Append word to last position in list
-			System.out.println(wordList.add(appendWord));
+				System.out.println(wordList);
 
-			System.out.println(wordList);
+			} else {
+				System.out.println(wordList);
+				wordList.add(findWord);
+				wordList.stream().forEach(word -> {
+					try {
+						bufferWriter.write(" " + word.toString());
 
-			// Returns the index position of the item
-			System.out.println(findIndexOfWord + " index is : " + wordList.indexOf(findIndexOfWord));
-
-			System.out.println(wordList.set(indexPosition, addWordOnIndex));
-			
-			System.out.println(wordList);
+					} catch (Exception exception) {
+						exception.printStackTrace();
+					}
+				});
+				bufferWriter.close();
+				System.out.println(wordList);
+				bufferWriter.close();
+			}
 
 		} catch (IOException exception) {
 			exception.printStackTrace();
