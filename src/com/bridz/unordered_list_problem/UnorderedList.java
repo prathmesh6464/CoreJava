b4c2d3e313413d674479b6339c83275bd46bdef2
prathamesh <prathmesh6464@gmail.com>
2020-05-29 13:55:14 +0530

    REFACTORED : Added main function added buffered reader feature.

--- src/com/bridz/unordered_list_problem/UnorderedList.java
+++ src/com/bridz/unordered_list_problem/UnorderedList.java
@@ -1,5 +1,34 @@
 package com.bridz.unordered_list_problem;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
 public class UnorderedList {
+
+	public static void main(String args[]) {
+
+		try {
+			BufferedReader bufferReader = new BufferedReader(new FileReader(
+					"C:\\Users\\King\\Documents\\workspace-spring-tool-suite-4-4.6.0.RELEASE\\CoreJava\\src\\UnorderedListOperationFile\\UnorderedOperationFile.txt"));
+
+			String fileLines;
+			List wordList = new LinkedList();
+			while ((fileLines = bufferReader.readLine()) != null) {
+				System.out.println(fileLines);
+				String[] splittedFileLines = fileLines.split(" ");
+				for (String eachWord : splittedFileLines) {
+					wordList.add(eachWord);
+					System.out.println(wordList);
+				}
+			}
+
+		} catch (IOException exception) {
+			exception.printStackTrace();
+		}
+
+	}
 
 }
